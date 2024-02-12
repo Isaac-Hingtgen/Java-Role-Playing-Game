7b8b1b0a4745410777bf39bc97514957d0866c44
Isaac Hingtgen <ihingtge@asu.edu>
2024-02-11 17:37:08 -0700

    Initial commit of basic setup

---
+++ src/main/java/ser316/rpg/Start.java
@@ -0,0 +1,11 @@
+package main.java.ser316.rpg;
+
+public class Start {
+    public static void main(String [] argv) {
+        System.out.println("Hello World");
+    }
+
+    public int foo(int num) {
+        return num;
+    }
+}
